0a599a73d29dfbc643675e3df909c8e24a2314b9
David Kisanga <davidlivingstonekisanga@gmail.com>
2022-09-09 06:05:40 -0500

    fixed SSL Certificate_verify_failed bug when using python 3.6+

--- Sprouts.py
+++ Sprouts.py
@@ -47,7 +47,7 @@ poly_list = []
 
 n = None
 
-# For making an .exe
+# # For making a .exe
 # def resource_path(relative_path):
 #     """ Get absolute path to resource, works for dev and for PyInstaller """
 #     try:
@@ -55,7 +55,6 @@ n = None
 #         base_path = sys._MEIPASS
 #     except Exception:
 #         base_path = os.path.abspath(".")
-
 #     return os.path.join(base_path, relative_path)
 
 
